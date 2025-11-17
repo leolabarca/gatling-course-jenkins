f8b3caa4b8d8963d8aab18d9ab60209d4cdbe1f2
leolabarca <leolabarca@gmail.com>
2025-11-16 21:38:59 -0300

    first commit

--- src/test/scala/videogamedb/simulations/BasicLoadSimulation.scala
+++ src/test/scala/videogamedb/simulations/BasicLoadSimulation.scala
@@ -2,27 +2,28 @@ package videogamedb.simulations
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
 class BasicLoadSimulation extends Simulation {
 
-  val httpProtocol = http.baseUrl("https://videogamedb.uk/api")
+  val httpProtocol = http
+    .baseUrl("https://videogamedb.uk/api")
     .acceptHeader("application/json")
 
-  def getAllVideoGames() = {
+  def getAllVideoGames() =
     exec(
       http("Get all video games")
         .get("/videogame")
     )
-  }
 
-  def getSpecificGame() = {
+  def getSpecificGame() =
     exec(
       http("Get specific game")
         .get("/videogame/2")
     )
-  }
 
   val scn = scenario("Basic Load Simulation")
+    .pause(10)                 // 👈 “silencio” inicial 10 s
     .exec(getAllVideoGames())
     .pause(5)
     .exec(getSpecificGame())
@@ -31,10 +32,9 @@ class BasicLoadSimulation extends Simulation {
 
   setUp(
     scn.inject(
-      nothingFor(5),
-      atOnceUsers(5),
-      rampUsers(10).during(10)
+      rampConcurrentUsers(1).to(50).during(30.seconds),   // sube 1 → 50
+      rampConcurrentUsers(50).to(20).during(30.seconds),  // baja 50 → 20
+      constantConcurrentUsers(20).during(60.seconds)      // 20 conc. por 60 s
     ).protocols(httpProtocol)
   )
-
 }
